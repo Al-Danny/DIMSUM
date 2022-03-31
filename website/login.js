35366c80fb17eb6d1e0a74abb158004fb2a41c9f
19Ars <20081010014@student.upnjatim.ac.id>
2022-03-31 09:25:17 +0700

    tabel

--- website/login.js
+++ website/login.js
@@ -5,7 +5,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 if ( username == "admin" && password == "admin123"){
 alert ("Login successfully");
-window.location = "distribusi1.html"; // Redirecting to other page.
+window.location = "distribusi1admin.html"; // Redirecting to other page.
 return false;
 }
 else{
