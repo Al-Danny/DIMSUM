035c41f9d0b38a44789dcb25f1703d8d88fdba49
Al-Danny <aldannyrian123@gmail.com>
2022-04-06 18:20:16 +0700

    fix login

--- website/search.js
+++ website/search.js
@@ -1,21 +1,11 @@
-function myFunction() {
-  // Declare variables
-  var input, filter, table, tr, td, i, txtValue;
-  input = document.getElementById("myInput");
-  filter = input.value.toUpperCase();
-  table = document.getElementById("");
-  tr = table.getElementsByTagName("tr");
+function search() {
+  let textToSearch = document.getElementById("input").value;
+  let paragraph = document.getElementById("p");
 
-  // Loop through all table rows, and hide those who don't match the search query
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }
-  }
+  console.log = textToSearch.replace(/[.*+?^${}()|[\]\\]/g,"\\$&");
+
+  let pattern = new RegExp(`${textToSearch}`,"gi");
+
+  paragraph.innerHTML = paragraph.textContent.replace(pattern, match => `<mark>${match}</mark>`)
+
 }
